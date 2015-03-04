360eb2358d7dcf8a6dfbf697be26234332099660
Sebastian Fabian <sjlfabian@gmail.com>
2015-03-04 12:31:20 +0100

    Removed dead code

--- src/sunny.c
+++ src/sunny.c
@@ -23,30 +23,6 @@ static void sunnygfx_day_update(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorYellow);
   graphics_context_set_stroke_color(ctx, GColorIcterine);
   graphics_fill_circle(ctx, GPoint(72, -30), 80);
-    
-  return;
-
-  for (i = 0; i < 10; i++){
-    if (i == 9){
-      if (sunnygfx_offset > 0) {
-        graphics_context_set_stroke_color(ctx, GColorInchworm);
-      }
-      if (sunnygfx_offset > 1) {
-        graphics_context_set_stroke_color(ctx, GColorMintGreen);
-      }
-      if (sunnygfx_offset > 2) {
-        graphics_context_set_stroke_color(ctx, GColorMediumAquamarine);
-      }
-      if (sunnygfx_offset > 3) {
-        graphics_context_set_stroke_color(ctx, GColorCyan);
-      }
-    }
-    graphics_draw_circle(ctx, GPoint(72, -30), 80 + (i * 10)+sunnygfx_offset*2);
-  }
-  sunnygfx_offset++;
-  if (sunnygfx_offset > 4) {
-    sunnygfx_offset = 0;
-  }
 }
 
 static void sunnygfx_night_update(Layer *layer, GContext *ctx) {
